ab7986d558364d904aa483b87184f8115a4c21c5
Luis Manuel <luis.manuel.saenz@hotmail.com>
2018-05-08 11:39:36 -0600

    Creacion de Carpetas e Inicio

--- ActivoFijo/Inicio.Designer.cs
+++ ActivoFijo/Inicio.Designer.cs
@@ -28,13 +28,274 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.Catalogos = new System.Windows.Forms.ToolStripMenuItem();
+            this.Areas = new System.Windows.Forms.ToolStripMenuItem();
+            this.SubDirecciones = new System.Windows.Forms.ToolStripMenuItem();
+            this.Departamentos = new System.Windows.Forms.ToolStripMenuItem();
+            this.Empleados = new System.Windows.Forms.ToolStripMenuItem();
+            this.Vehiculos = new System.Windows.Forms.ToolStripMenuItem();
+            this.Proveedores = new System.Windows.Forms.ToolStripMenuItem();
+            this.Articulos = new System.Windows.Forms.ToolStripMenuItem();
+            this.Marcas = new System.Windows.Forms.ToolStripMenuItem();
+            this.Familias = new System.Windows.Forms.ToolStripMenuItem();
+            this.Activo = new System.Windows.Forms.ToolStripMenuItem();
+            this.Bienes = new System.Windows.Forms.ToolStripMenuItem();
+            this.BienesSF = new System.Windows.Forms.ToolStripMenuItem();
+            this.ImprimirEtiquetas = new System.Windows.Forms.ToolStripMenuItem();
+            this.BienesBaja = new System.Windows.Forms.ToolStripMenuItem();
+            this.Reportes = new System.Windows.Forms.ToolStripMenuItem();
+            this.Historico = new System.Windows.Forms.ToolStripMenuItem();
+            this.ValorBienes = new System.Windows.Forms.ToolStripMenuItem();
+            this.Configuracion = new System.Windows.Forms.ToolStripMenuItem();
+            this.ConfigurarImpresora = new System.Windows.Forms.ToolStripMenuItem();
+            this.PanelBase = new System.Windows.Forms.Panel();
+            this.LogoLocal = new System.Windows.Forms.PictureBox();
+            this.LogoEstatal = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            this.PanelBase.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoLocal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoEstatal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.menuStrip1.Font = new System.Drawing.Font("Times New Roman", 11.25F);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Catalogos,
+            this.Activo,
+            this.Reportes,
+            this.Configuracion});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1362, 25);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Catalogos
+            // 
+            this.Catalogos.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Areas,
+            this.SubDirecciones,
+            this.Departamentos,
+            this.Empleados,
+            this.Vehiculos,
+            this.Proveedores,
+            this.Articulos,
+            this.Marcas,
+            this.Familias});
+            this.Catalogos.Name = "Catalogos";
+            this.Catalogos.Size = new System.Drawing.Size(78, 21);
+            this.Catalogos.Text = "Catalogos";
+            // 
+            // Areas
+            // 
+            this.Areas.Name = "Areas";
+            this.Areas.Size = new System.Drawing.Size(197, 22);
+            this.Areas.Text = "Areas o Direcciones";
+            // 
+            // SubDirecciones
+            // 
+            this.SubDirecciones.Name = "SubDirecciones";
+            this.SubDirecciones.Size = new System.Drawing.Size(197, 22);
+            this.SubDirecciones.Text = "SubDirecciones";
+            // 
+            // Departamentos
+            // 
+            this.Departamentos.Name = "Departamentos";
+            this.Departamentos.Size = new System.Drawing.Size(197, 22);
+            this.Departamentos.Text = "Departamentos ";
+            // 
+            // Empleados
+            // 
+            this.Empleados.Name = "Empleados";
+            this.Empleados.Size = new System.Drawing.Size(197, 22);
+            this.Empleados.Text = "Empleados";
+            // 
+            // Vehiculos
+            // 
+            this.Vehiculos.Name = "Vehiculos";
+            this.Vehiculos.Size = new System.Drawing.Size(197, 22);
+            this.Vehiculos.Text = "Vehiculos";
+            // 
+            // Proveedores
+            // 
+            this.Proveedores.Name = "Proveedores";
+            this.Proveedores.Size = new System.Drawing.Size(197, 22);
+            this.Proveedores.Text = "Proveedores";
+            // 
+            // Articulos
+            // 
+            this.Articulos.Name = "Articulos";
+            this.Articulos.Size = new System.Drawing.Size(197, 22);
+            this.Articulos.Text = "Articulos";
+            // 
+            // Marcas
+            // 
+            this.Marcas.Name = "Marcas";
+            this.Marcas.Size = new System.Drawing.Size(197, 22);
+            this.Marcas.Text = "Marcas";
+            // 
+            // Familias
+            // 
+            this.Familias.Name = "Familias";
+            this.Familias.Size = new System.Drawing.Size(197, 22);
+            this.Familias.Text = "Familias";
+            // 
+            // Activo
+            // 
+            this.Activo.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Bienes,
+            this.BienesSF,
+            this.ImprimirEtiquetas,
+            this.BienesBaja});
+            this.Activo.Name = "Activo";
+            this.Activo.Size = new System.Drawing.Size(59, 21);
+            this.Activo.Text = "Activo";
+            // 
+            // Bienes
+            // 
+            this.Bienes.Name = "Bienes";
+            this.Bienes.Size = new System.Drawing.Size(200, 22);
+            this.Bienes.Text = "Bienes";
+            // 
+            // BienesSF
+            // 
+            this.BienesSF.Name = "BienesSF";
+            this.BienesSF.Size = new System.Drawing.Size(200, 22);
+            this.BienesSF.Text = "Bienes sin Factura";
+            // 
+            // ImprimirEtiquetas
+            // 
+            this.ImprimirEtiquetas.Name = "ImprimirEtiquetas";
+            this.ImprimirEtiquetas.Size = new System.Drawing.Size(200, 22);
+            this.ImprimirEtiquetas.Text = "Imprimir Etiquetas";
+            // 
+            // BienesBaja
+            // 
+            this.BienesBaja.Name = "BienesBaja";
+            this.BienesBaja.Size = new System.Drawing.Size(200, 22);
+            this.BienesBaja.Text = "Bienes dados de baja";
+            // 
+            // Reportes
+            // 
+            this.Reportes.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Historico,
+            this.ValorBienes});
+            this.Reportes.Name = "Reportes";
+            this.Reportes.Size = new System.Drawing.Size(73, 21);
+            this.Reportes.Text = "Reportes";
+            // 
+            // Historico
+            // 
+            this.Historico.Name = "Historico";
+            this.Historico.Size = new System.Drawing.Size(181, 22);
+            this.Historico.Text = "Historico del Bien";
+            // 
+            // ValorBienes
+            // 
+            this.ValorBienes.Name = "ValorBienes";
+            this.ValorBienes.Size = new System.Drawing.Size(181, 22);
+            this.ValorBienes.Text = "Valor en Bienes";
+            // 
+            // Configuracion
+            // 
+            this.Configuracion.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ConfigurarImpresora});
+            this.Configuracion.Name = "Configuracion";
+            this.Configuracion.Size = new System.Drawing.Size(102, 21);
+            this.Configuracion.Text = "Configuración";
+            // 
+            // ConfigurarImpresora
+            // 
+            this.ConfigurarImpresora.Name = "ConfigurarImpresora";
+            this.ConfigurarImpresora.Size = new System.Drawing.Size(212, 22);
+            this.ConfigurarImpresora.Text = "Impresora de Etiquetas";
+            // 
+            // PanelBase
+            // 
+            this.PanelBase.BackColor = System.Drawing.Color.Transparent;
+            this.PanelBase.Controls.Add(this.LogoLocal);
+            this.PanelBase.Controls.Add(this.LogoEstatal);
+            this.PanelBase.Location = new System.Drawing.Point(0, 24);
+            this.PanelBase.Name = "PanelBase";
+            this.PanelBase.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.PanelBase.Size = new System.Drawing.Size(1388, 681);
+            this.PanelBase.TabIndex = 2;
+            // 
+            // LogoLocal
+            // 
+            this.LogoLocal.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.LogoLocal.Location = new System.Drawing.Point(1056, 38);
+            this.LogoLocal.Name = "LogoLocal";
+            this.LogoLocal.Size = new System.Drawing.Size(231, 273);
+            this.LogoLocal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.LogoLocal.TabIndex = 4;
+            this.LogoLocal.TabStop = false;
+            // 
+            // LogoEstatal
+            // 
+            this.LogoEstatal.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.LogoEstatal.Location = new System.Drawing.Point(992, 335);
+            this.LogoEstatal.Name = "LogoEstatal";
+            this.LogoEstatal.Size = new System.Drawing.Size(346, 273);
+            this.LogoEstatal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.LogoEstatal.TabIndex = 3;
+            this.LogoEstatal.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1362, 693);
+            this.Controls.Add(this.PanelBase);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1382, 764);
+            this.MinimumSize = new System.Drawing.Size(1364, 736);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sistema de Activo Fijo";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.PanelBase.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.LogoLocal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoEstatal)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem Catalogos;
+        private System.Windows.Forms.ToolStripMenuItem Areas;
+        private System.Windows.Forms.ToolStripMenuItem SubDirecciones;
+        private System.Windows.Forms.ToolStripMenuItem Departamentos;
+        private System.Windows.Forms.ToolStripMenuItem Empleados;
+        private System.Windows.Forms.ToolStripMenuItem Vehiculos;
+        private System.Windows.Forms.ToolStripMenuItem Proveedores;
+        private System.Windows.Forms.ToolStripMenuItem Articulos;
+        private System.Windows.Forms.ToolStripMenuItem Marcas;
+        private System.Windows.Forms.ToolStripMenuItem Familias;
+        private System.Windows.Forms.ToolStripMenuItem Activo;
+        private System.Windows.Forms.ToolStripMenuItem Bienes;
+        private System.Windows.Forms.ToolStripMenuItem BienesSF;
+        private System.Windows.Forms.ToolStripMenuItem ImprimirEtiquetas;
+        private System.Windows.Forms.ToolStripMenuItem BienesBaja;
+        private System.Windows.Forms.ToolStripMenuItem Reportes;
+        private System.Windows.Forms.ToolStripMenuItem Historico;
+        private System.Windows.Forms.ToolStripMenuItem ValorBienes;
+        private System.Windows.Forms.ToolStripMenuItem Configuracion;
+        private System.Windows.Forms.ToolStripMenuItem ConfigurarImpresora;
+        private System.Windows.Forms.Panel PanelBase;
+        private System.Windows.Forms.PictureBox LogoLocal;
+        private System.Windows.Forms.PictureBox LogoEstatal;
     }
 }
 
