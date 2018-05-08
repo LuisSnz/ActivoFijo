e868864f33e6b51a3ff92bb629ff9260fac4edc2
Luis Manuel <luis.manuel.saenz@hotmail.com>
2018-05-08 12:32:53 -0600

    Creacion de Clases

---
+++ ActivoFijo/Clases/Etiquetas.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Printing;
+using BarcodeLib;
+using System.Data;
+using System.IO;
+
+namespace ActivoFijo.Clases
+{
+    class Etiquetas
+    {
+        public static void ImprimirEtiquetas(string min, string max)
+        {
+            try
+            {
+                PrintDocument pd = new PrintDocument();
+                pd.PrinterSettings.PrinterName = (Clases.Variables.ConfImpresora);
+                DataSet ds = Clases.LLenadoGrids.GenerarDataSet("select bienes.Etiqueta, CatArticulos.Descripcion" +
+                        " from bienes inner join CatArticulos on bienes.IdArticulo = CatArticulos.Id where " +
+                        "bienes.Etiqueta between " + min + " and " + max, "bienes");
+                for (int x = 0; x < ds.Tables[0].Rows.Count; x++)
+                {
+                    Clases.Variables.IEtiqueta = ds.Tables[0].Rows[x][0].ToString();
+                    Clases.Variables.IArticulo = ds.Tables[0].Rows[x][1].ToString();
+                    pd.PrintPage += new PrintPageEventHandler(documentoAimprimir);
+                    pd.Print();
+                }
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show("Printing " + exp.Message);
+            }
+
+        }
+
+        public static void documentoAimprimir(object sender, PrintPageEventArgs e)
+        {
+            try
+            {
+                StringFormat stringFormat = new StringFormat();
+                stringFormat.Alignment = StringAlignment.Center;
+                stringFormat.LineAlignment = StringAlignment.Center;
+                Font fnt = new Font("Times New Roman", 12);
+                string texto = string.Format("JMAS CAMARGO");
+                e.Graphics.DrawString(texto, fnt, Brushes.Black, 125, 15, stringFormat);
+                fnt = new Font("Times New Roman", 7);
+                texto = string.Format(Clases.Variables.IArticulo);
+                e.Graphics.DrawString(texto, fnt, Brushes.Black, 125, 30, stringFormat);
+                Barcode code = new Barcode();
+                Image codigo = code.Encode(TYPE.CODE128, Clases.Variables.IEtiqueta, Color.Black, Color.White, 220, 70);
+                e.Graphics.DrawImage(codigo, 45, 40, 160, 65);
+                fnt = new Font("Times New Roman", 8);
+                texto = string.Format(Clases.Variables.IEtiqueta);
+                e.Graphics.DrawString(texto, fnt, Brushes.Black, 125, 116, stringFormat);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Printpage" + ex.Message);
+            }
+        }
+        public static void llamarVariableEtiqueta()
+        {
+            String line;
+            try
+            {
+                string ruta = Path.Combine(Application.StartupPath, "ImpresoraEtiquetas.txt");
+                StreamReader sr = new StreamReader(ruta);
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Clases.Variables.ConfImpresora = line;
+                    sr.ReadLine();
+                }
+                sr.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error:" + ex.ToString());
+            }
+        }
+    }
+}
---
+++ ActivoFijo/Clases/Variables.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivoFijo.Clases
+{
+    class Variables
+    {
+        //String de conexion a la base de datos
+        public static string scon = ("Data Source=.;Initial Catalog=ActivoJcas;User ID=JMAS;Password=qwerty");
+        //Variable de busqueda de grid
+        public static string ConsultaBuscar;
+        //familias
+        public static string IDFamilia;
+        public static string DescripcionFamilia;
+        //marcas
+        public static string IDMarcas;
+        public static string DescripcionMarcas;
+        //areas
+        public static string IdAreas;
+        public static string DescripcionAreas;
+        //Departamentos
+        public static string IdDeptos;
+        public static string DescripcionDeptos;
+        public static string DireccionDeptos;
+        public static string SubDeptos;
+        //Empleados
+        public static string IdEmpleados;
+        public static string EmpleadosBaja;
+        public static string EmpleadosBloqueado;
+        public static string EmpleadosPliegos;
+        public static string EmpleadosNoPliegos;
+        public static string EmpleadosJefe;
+        public static string EmpleadosNombre;
+        public static string EmpleadosDepto;
+        public static string EmpleadosLicencia;
+        public static string EmpleadosFecha;
+        public static string EmpleadosNombreP;
+        public static string EmpleadosNumJefe;
+        //proveedores
+        public static string IdProveedores;
+        //SubDireccion
+        public static string IdSubD;
+        public static string DescripcionSubD;
+        //Bienes Dados de Baja
+        public static string IdBienesB;
+        public static string BienesBMotivoBaja;
+        public static string BienesBObservacion;
+        public static string BienesBValor;
+        public static string MotivoBaja;
+        //CatArticulos
+        public static string IdArticulo;
+        public static string ArticuloDescripcion;
+        public static string ArticuloContrato;
+        public static string ArticuloInventariable;
+        public static string ArticuloFamilia;
+        public static string ArticuloMedida;
+        public static string ArticuloTipo;
+        //Bienes
+        public static string IdBienes;
+        public static string BienesEtiqueta;
+        public static string BienesOrdenCompra;
+        public static string BienesFactura;
+        public static string BienesTotal;
+        public static string BienesFamilia;
+        public static string BienesDescripcionArticulo;
+        public static string BienesEmpleado;
+        public static string BienesDepartamento;
+        public static string BienesConsumible;
+        public static string BienesObservacion;
+        //BienesSF
+        public static string IdBienesSF;
+        public static string BienesSFEtiqueta;
+        public static string BienesSFOrdenCompra;
+        public static string BienesSFTotal;
+        public static string BienesSFFamilia;
+        public static string BienesSFDescripcionArticulo;
+        public static string BienesSFEmpleado;
+        public static string BienesSFDepartamento;
+        public static string BienesSFConsumible;
+        public static string BienesSFObservacion;
+        //Imrimir Etiquetas
+        public static string IEtiqueta;
+        public static string IArticulo;
+        //Login
+        public static string Usuario = "x";
+        //Configurar impresora
+        public static string ConfImpresora;
+        //Escala de grids
+        public static int GridWidth;
+        public static int GridHeight;
+
+    }
+}
