4baa01f685c5edb13f3c88862bbfcd1a37575b5b
jose eduardo carmona vasquez <LALO_1095@live.com.mx>
2018-05-29 10:33:05 -0600

    * s

--- ActivoFijo/Catalogos/Proveedores/Nuevo.Designer.cs
+++ ActivoFijo/Catalogos/Proveedores/Nuevo.Designer.cs
@@ -134,21 +134,21 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(64, 39);
+            this.label2.Location = new System.Drawing.Point(56, 36);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(59, 17);
+            this.label2.Size = new System.Drawing.Size(66, 17);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Nombre:";
+            this.label2.Text = "*Nombre:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(34, 120);
+            this.label5.Location = new System.Drawing.Point(16, 120);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(88, 17);
+            this.label5.Size = new System.Drawing.Size(106, 17);
             this.label5.TabIndex = 5;
-            this.label5.Text = "Fax/Telefono:";
+            this.label5.Text = "*Fax o Telefono:";
             // 
             // label1
             // 
@@ -164,11 +164,11 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(71, 147);
+            this.label6.Location = new System.Drawing.Point(64, 147);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(52, 17);
+            this.label6.Size = new System.Drawing.Size(59, 17);
             this.label6.TabIndex = 6;
-            this.label6.Text = "Ciudad:";
+            this.label6.Text = "*Ciudad:";
             // 
             // label7
             // 
