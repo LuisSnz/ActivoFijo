4e5f1a45f5f6070534b73277ccaa8d8a96655132
Luis Manuel <luis.manuel.saenz@hotmail.com>
2018-06-04 11:39:28 -0600

    correccion de nombres

--- ActivoFijo/Activo/ImprimirEtiquetas.cs
+++ ActivoFijo/Activo/ImprimirEtiquetas.cs
@@ -19,7 +19,7 @@ namespace ActivoFijo.Activo
 
         private void Imprimir_Click(object sender, EventArgs e)
         {
-            Clases.Etiquetas.llamarVariableEtiqueta();
+            Clases.Etiquetas.Impresora();
             if (Clases.Variables.ConfImpresora == null)
                 MessageBox.Show("Impresora no asignada, porfavor dirijase a (Configuracion/Impresora de etiqueta) y especifique el nombre de la impresora");
             else
