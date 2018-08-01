e608f7796815c8ef0455f0b1040efc0a3f096012
Luis Manuel <luis.manuel.saenz@hotmail.com>
2018-08-01 10:17:06 -0600

    contraseña

--- ActivoFijo/Auxiliares/ContraseñaBD.Designer.cs
+++ ActivoFijo/Auxiliares/ContraseñaBD.Designer.cs
@@ -37,23 +37,23 @@
             // 
             // label1
             // 
-            this.label1.BackColor = System.Drawing.SystemColors.Control;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(0, 9);
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 18);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(251, 57);
+            this.label1.Size = new System.Drawing.Size(225, 43);
             this.label1.TabIndex = 8;
-            this.label1.Text = "Ingrese la contraseña para accesar\r\na la modificacion de la cadena de conexion";
+            this.label1.Text = "Ingrese la contraseña para accesar\r\na esta configuracion";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Cancelar
             // 
             this.Cancelar.BackColor = System.Drawing.Color.White;
             this.Cancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.Cancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cancelar.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Cancelar.Location = new System.Drawing.Point(139, 107);
             this.Cancelar.Name = "Cancelar";
-            this.Cancelar.Size = new System.Drawing.Size(86, 27);
+            this.Cancelar.Size = new System.Drawing.Size(86, 38);
             this.Cancelar.TabIndex = 3;
             this.Cancelar.Text = "Cancelar";
             this.Cancelar.UseVisualStyleBackColor = false;
@@ -62,10 +62,10 @@
             // Aceptar
             // 
             this.Aceptar.BackColor = System.Drawing.Color.White;
-            this.Aceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Aceptar.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Aceptar.Location = new System.Drawing.Point(29, 107);
             this.Aceptar.Name = "Aceptar";
-            this.Aceptar.Size = new System.Drawing.Size(86, 27);
+            this.Aceptar.Size = new System.Drawing.Size(86, 38);
             this.Aceptar.TabIndex = 2;
             this.Aceptar.Text = "Aceptar";
             this.Aceptar.UseVisualStyleBackColor = false;
@@ -73,10 +73,10 @@
             // 
             // Contraseña
             // 
-            this.Contraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Contraseña.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Contraseña.Location = new System.Drawing.Point(29, 69);
             this.Contraseña.Name = "Contraseña";
-            this.Contraseña.Size = new System.Drawing.Size(196, 23);
+            this.Contraseña.Size = new System.Drawing.Size(196, 25);
             this.Contraseña.TabIndex = 1;
             this.Contraseña.UseSystemPasswordChar = true;
             // 
@@ -85,6 +85,7 @@
             this.AcceptButton = this.Aceptar;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DodgerBlue;
             this.ClientSize = new System.Drawing.Size(255, 157);
             this.ControlBox = false;
             this.Controls.Add(this.Contraseña);
