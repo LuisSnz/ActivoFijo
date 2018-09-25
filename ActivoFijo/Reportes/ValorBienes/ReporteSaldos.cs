07ded44c6c9c7111b37dec0cfc8b82d42b6cfb63
jose eduardo carmona vasquez <LALO_1095@live.com.mx>
2018-09-25 10:24:33 -0600

    reporte saldo inicial

---
+++ ActivoFijo/Reportes/ValorBienes/ReporteSaldos.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Reporting.WinForms;
+
+
+namespace ActivoFijo.Reportes.ValorBienes
+{
+    public partial class ReporteSaldos : Form
+    {
+        public ReporteSaldos()
+        {
+            InitializeComponent();
+        }
+
+        private void ReporteSaldos_Load(object sender, EventArgs e)
+        {
+            ReportDataSource rds = new ReportDataSource();
+            rds.Name = "DataSet1";
+            rds.Value = Clases.LLenadoGrids.GenerarDataSet(Clases.Variables.ConsultaBuscar, "bienes").Tables[0];
+            this.reportViewer1.LocalReport.DataSources.Clear();
+            this.reportViewer1.LocalReport.DataSources.Add(rds);
+
+            ReportParameter[] reporte = new ReportParameter[1];
+            reporte[0] = new ReportParameter("total", Clases.Variables.Total);
+            reportViewer1.LocalReport.SetParameters(reporte);
+            
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
